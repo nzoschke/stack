e1fe6495f5659888cd2486966f9cbe21b4add6b0
Noah Zoschke <nzoschke@gmail.com>
2015-06-20 16:47:34 -0700

    gofmt; test Resource Settings Type

--- formation_test.go
+++ formation_test.go
@@ -7,30 +7,41 @@ import (
 	"testing"
 )
 
-func TestEmptyVersion(t *testing.T) {
-  tmpl := _template(t, nil)
+func TestVersion(t *testing.T) {
+	tmpl := _template(t, nil)
 
-	if tmpl.AWSTemplateFormatVersion != "2010-09-09" {
-		t.Errorf("AWSTemplateFormatVersion got %s, want %s", tmpl.AWSTemplateFormatVersion, "2010-09-09")
+	want := "2010-09-09"
+
+	if tmpl.AWSTemplateFormatVersion != want {
+		t.Errorf("TestVersion got %s, want %s", tmpl.AWSTemplateFormatVersion, want)
 	}
 
 }
 
-func TestEmptyResources(t *testing.T) {
-  tmpl := _template(t, nil)
+func TestResources(t *testing.T) {
+	tmpl := _template(t, nil)
+	resources := tmpl.Resources
 
-  var keys sort.StringSlice = make([]string, 0, len(tmpl.Resources))
+	var keys sort.StringSlice = make([]string, 0, len(resources))
 
-  for k := range tmpl.Resources {
-    keys = append(keys, k)
-  }
-  keys.Sort()
+	for k := range resources {
+		keys = append(keys, k)
+	}
+	keys.Sort()
 
-  var want sort.StringSlice = []string{"DynamoBuilds", "DynamoChanges", "DynamoReleases", "ServiceRole", "Settings"}
+	var want sort.StringSlice = []string{"DynamoBuilds", "DynamoChanges", "DynamoReleases", "ServiceRole", "Settings"}
 
-  if !reflect.DeepEqual(keys, want) {
-    t.Errorf("Resources got %s, want %s", keys, want)
-  }
+	if !reflect.DeepEqual(keys, want) {
+		t.Errorf("TestResources got %s, want %s", keys, want)
+	}
+
+	s := (resources["Settings"]).(map[string]interface{})
+	got := s["Type"]
+	w := "AWS::S3::Bucket"
+
+	if got != w {
+		t.Errorf("TestResources got %s, want %s", keys, w)
+	}
 }
 
 func TestCelery(t *testing.T) {
@@ -52,19 +63,19 @@ func TestJSONEvaluation(t *testing.T) {
 
 }
 
-func _template(t *testing.T, data interface{}) (*Template) {
-  j, err := buildTemplate(data)
+func _template(t *testing.T, data interface{}) *Template {
+	j, err := buildTemplate(data)
 
-  if err != nil {
-    t.Errorf("_template err: %s", err)
-  }
+	if err != nil {
+		t.Errorf("_template err: %s", err)
+	}
 
-  var tmpl Template
-  err = json.Unmarshal([]byte(j), &tmpl)
+	var tmpl Template
+	err = json.Unmarshal([]byte(j), &tmpl)
 
-  if err != nil {
-    t.Errorf("_template err: %s", err)
-  }
+	if err != nil {
+		t.Errorf("_template err: %s", err)
+	}
 
-  return &tmpl
+	return &tmpl
 }
