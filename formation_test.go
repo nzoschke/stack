1990a513533be09894b59e347158fcec4e5a4879
Noah Zoschke <nzoschke@gmail.com>
2015-06-20 16:34:49 -0700

    refactor test functions

--- formation_test.go
+++ formation_test.go
@@ -7,38 +7,30 @@ import (
 	"testing"
 )
 
-func TestEmptyManifest(t *testing.T) {
-	var manifest Manifest
-
-	j, err := buildTemplate(manifest)
-
-	if err != nil {
-		t.Errorf("TestEmptyManifest buildTemplate err: %s", err)
-	}
-
-	var tmpl Template
-	err = json.Unmarshal([]byte(j), &tmpl)
-
-	if err != nil {
-		t.Errorf("TestEmptyManifest json.Unmarshal err: %s", err)
-	}
+func TestEmptyVersion(t *testing.T) {
+  tmpl := _template(t, nil)
 
 	if tmpl.AWSTemplateFormatVersion != "2010-09-09" {
 		t.Errorf("AWSTemplateFormatVersion got %s, want %s", tmpl.AWSTemplateFormatVersion, "2010-09-09")
 	}
 
-	var keys sort.StringSlice = make([]string, 0, len(tmpl.Resources))
+}
 
-	for k := range tmpl.Resources {
-		keys = append(keys, k)
-	}
-	keys.Sort()
+func TestEmptyResources(t *testing.T) {
+  tmpl := _template(t, nil)
 
-	var want sort.StringSlice = []string{"DynamoBuilds", "DynamoChanges", "DynamoReleases", "ServiceRole", "Settings"}
+  var keys sort.StringSlice = make([]string, 0, len(tmpl.Resources))
 
-	if !reflect.DeepEqual(keys, want) {
-		t.Errorf("Resources got %s, want %s", keys, want)
-	}
+  for k := range tmpl.Resources {
+    keys = append(keys, k)
+  }
+  keys.Sort()
+
+  var want sort.StringSlice = []string{"DynamoBuilds", "DynamoChanges", "DynamoReleases", "ServiceRole", "Settings"}
+
+  if !reflect.DeepEqual(keys, want) {
+    t.Errorf("Resources got %s, want %s", keys, want)
+  }
 }
 
 func TestCelery(t *testing.T) {
@@ -58,4 +50,21 @@ func TestDockerCompose(t *testing.T) {
 
 func TestJSONEvaluation(t *testing.T) {
 
+}
+
+func _template(t *testing.T, data interface{}) (*Template) {
+  j, err := buildTemplate(data)
+
+  if err != nil {
+    t.Errorf("_template err: %s", err)
+  }
+
+  var tmpl Template
+  err = json.Unmarshal([]byte(j), &tmpl)
+
+  if err != nil {
+    t.Errorf("_template err: %s", err)
+  }
+
+  return &tmpl
 }
